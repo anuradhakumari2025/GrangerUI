6cf01f9ceb4dfce5d11d54b99709d1a5558eaec9
anuradhakumari2025 <krianuradha4321@gmail.com>
2024-07-21 07:58:50 +0530

    added createContext and useReducer

---
+++ src/Store/post-list-store.jsx
@@ -0,0 +1,30 @@
+import { createContext, useReducer } from "react";
+
+const PostList = createContext(
+  {
+    postList: [],
+    addPost : () =>{},
+    deletePost : () => {}
+  }
+  
+);
+const PostListProvider =({children})=> {
+  const [postList,dispatchPostList] = useReducer([]);
+
+  const addPost = () => {
+
+  }
+  const deletePost = () => {
+
+  }
+  return(
+    <PostList.Provider value={{
+      postList : postList,
+      addPost : addPost,
+      deletePost : deletePost
+    }
+    }>{children}</PostList.Provider>
+  )
+}
+
+export default PostListProvider;
